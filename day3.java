84cb25c31e490a4b1d1f92ccc2e69d8cc55e3da6
charan764 <119785206+charan764@users.noreply.github.com>
2026-04-08 11:57:02 +0530

    Add files via upload
    file 3 upload

---
+++ day3.java
@@ -0,0 +1,37 @@
+class day3
+     {
+
+        public static void main()
+        {
+               System.out.println("besant");
+                       day3 m = new day3();
+                        m.c1();             
+        }
+        
+        public void c1()
+        
+        {
+              System.out.println("technology");
+                   c2();
+         }
+         
+         public void c2()
+       
+        {
+           
+                System.out.println("java");
+
+                      c3();   
+         }
+       
+        public static void c3()
+       
+        {
+          
+                System.out.println("full stack");
+        }
+ 
+
+
+}
+        
