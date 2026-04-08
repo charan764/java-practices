1728b383eb62517dd3b38d762cc309af0f6b0a82
charan764 <119785206+charan764@users.noreply.github.com>
2026-04-08 11:55:46 +0530

    Add files via upload
    file day2 upload

---
+++ day2.java
@@ -0,0 +1,24 @@
+class day2
+{ 
+        public static void main(String[] args)
+        {
+               System.out.println("online games");
+                    day2 d= new day2();
+                    d.game1();
+                    game2();
+               System.out.println("pubg");
+                   
+        }
+
+        public void game1()
+        {
+               System.out.println("free fire");
+         
+        }
+        public static void game2()
+        {
+               System.out.println("call of duty");
+
+        }
+ 
+}
