5150280ae07b686f4b83cb0b733583ba4340e7d6
charan764 <119785206+charan764@users.noreply.github.com>
2026-04-08 11:53:22 +0530

    Add files via upload
    first file upload

---
+++ day1.java
@@ -0,0 +1,14 @@
+class day1{
+int x = 654;
+static int y = 321;
+
+public static void main(String[] args)
+{
+int z = 987;
+System.out.println(z);
+
+day1 s = new day1();
+System.out.println(s.x);
+System.out.println(day1.y);
+}
+}
